2007b94437447d49fb966bd01de2349d144189f8
Pavel Eliseev <deusagnis@gmail.com>
2025-04-05 12:22:27 +0300

    Improve response JSON encoding.

--- src/Middleware/ResponseEncoder.php
+++ src/Middleware/ResponseEncoder.php
@@ -6,6 +6,8 @@ use Closure;
 use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class ResponseEncoder
 {
@@ -37,20 +39,25 @@ class ResponseEncoder
     {
         $response = $next($request);
 
-        if (is_array($response)
-            or is_scalar($response)
-            or is_null($response)
-        ) {
-            $response = $this->factory->json($response);
-        } elseif (!$response instanceof JsonResponse) {
-            $response = $this->factory->json(
-                $response->content(),
-                $response->status(),
-                $response->headers->all()
-            );
+        if ($response instanceof JsonResponse) {
+            return $response;
+        }
+
+        if ($response instanceof BinaryFileResponse || $response->isRedirect()) {
+            return $response;
+        }
+
+        if ($response->getStatusCode() === Response::HTTP_NO_CONTENT) {
+            return response()->json(null, Response::HTTP_NO_CONTENT);
+        }
+
+
+        $data = $response->original ?? $response->getContent();
+
+        if (is_array($data) || is_scalar($data) || $data === null || $data instanceof \JsonSerializable) {
+            return response()->json($data, $response->getStatusCode(), $response->headers->all());
         }
 
         return $response;
-
     }
 }
